d8371a79c637fe6db00eb633f89f19e846a90806
nganb2014674 <nganb2014674@student.ctu.edu.vn>
2024-12-10 14:54:10 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
-import { createWebHistory, createWebHistory } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [{
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
-}, ];
+        path: "/",
+        name: "contactbook",
+        component: ContactBook,
+    },
+    {
+        path: "/:pathMatch(.*)*", // Route này sẽ khớp với tất cả các URL không hợp lệ
+        name: "notfound",
+        component: () =>
+            import ("@/views/NotFound.vue"), // Tải trang NotFound khi gặp lỗi 404
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(
